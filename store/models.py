a4de73694a91593be3ab08a449e0c432087b61fc
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-04-06 18:59:58 +0300

    made and run migrations

--- store/models.py
+++ store/models.py
@@ -15,6 +15,7 @@ class Collection(models.Model):
 
 class Product(models.Model):
     title = models.CharField(max_length=255)
+    slug = models.SlugField()
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.IntegerField()
@@ -69,6 +70,7 @@ class OrderItem(models.Model):
 class Address(models.Model):
     street = models.CharField(max_length=255)
     city = models.CharField(max_length=255)
+    zip = models.CharField(max_length=255, null=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
 
 
