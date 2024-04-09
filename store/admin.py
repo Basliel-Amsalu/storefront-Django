8c024ec3ec96d79fe27bb44862ed3a6d2aa38929
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-04-09 16:03:24 +0300

    "Add filters, actions, and fields to store admin; update list views"
    * Add a custom filter for product inventory
    * Add an action to clear inventory for selected products
    * Update product admin to display inventory status
    * Add a link to view orders for a customer in the customer admin
    * Update collection admin to display product count and add a search field.

--- store/admin.py
+++ store/admin.py
@@ -1,36 +1,90 @@
+from typing import Any
 from django.contrib import admin
+from django.db.models.query import QuerySet
+from django.http import HttpRequest
+from django.db.models import Count
+
+from django.urls import reverse
+from django.utils.html import format_html, urlencode
 
 from store import models
 
 
+class InventoryFilter(admin.SimpleListFilter):
+    title = "Inventory"
+    parameter_name = "inventory"
+
+    def lookups(self, request, model_admin):
+        return [("<10", "Low")]
+
+    def queryset(self, request, queryset):
+        if self.value() == "<10":
+            return queryset.filter(inventory__lt=10)
+
+
 @admin.register(models.Product)
 class ProductAdmin(admin.ModelAdmin):
+    autocomplete_fields = ["collection"]
+    prepopulated_fields = {"slug": ["title"]}
+    actions = ["clear_inventory"]
     list_display = ["title", "unit_price", "inventory_status", "collection"]
     list_editable = ["unit_price"]
+    list_filter = ["collection", "last_update", InventoryFilter]
 
     @admin.display(ordering="inventory")
     def inventory_status(self, product):
         if product.inventory < 10:
-            return "In Stock (Low)"
-        elif product.inventory >= 10:
-            return "In Stock"
-        else:
-            return "Out of Stock"
+            return "Low"
+        return "Okay"
+
+    @admin.action(description="Clear inventory")
+    def clear_inventory(self, request, queryset):
+        updated_count = queryset.update(inventory=0)
+        self.message_user(
+            request, f"{updated_count} products were successfully updated"
+        )
 
 
 @admin.register(models.Customer)
 class CustomerAdmin(admin.ModelAdmin):
-    list_display = ["first_name", "last_name", "membership"]
+    list_display = ["first_name", "last_name", "membership", "orders"]
     list_editable = ["membership"]
+    list_per_page = 10
+    ordering = ["first_name", "last_name"]
+    search_fields = ["first_name__istartswith", "last_name__istartswith"]
+
+    @admin.display(ordering="orders_count")
+    def orders(self, customer):
+        url = (
+            reverse("admin:store_order_changelist")
+            + "?"
+            + urlencode({"customer__id": str(customer.id)})
+        )
+        return format_html('<a href="{}">{} Orders</a>', url, customer.orders_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(orders_count=Count("order"))
 
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ["id", "placed_at", "customer_name"]
-    list_select_related = ["customer"]
-
-    def customer_name(self, order):
-        return order.customer.first_name
+    autocomplete_fields = ["customer"]
+    list_display = ["id", "placed_at", "customer"]
 
 
-admin.site.register(models.Collection)
+@admin.register(models.Collection)
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = ["title", "products_count"]
+    search_fields = ["title"]
+
+    @admin.display(ordering="products_count")
+    def products_count(self, collection):
+        url = (
+            reverse("admin:store_product_changelist")
+            + "?"
+            + urlencode({"collection__id": str(collection.id)})
+        )
+        return format_html("<a href={}>{}</a>", url, collection.products_count)
+
+    def get_queryset(self, request):
+        return super().get_queryset(request).annotate(products_count=Count("product"))
