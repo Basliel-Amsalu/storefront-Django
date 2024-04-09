c8cc5a23972276a3750c34ff4eeacc7881a99dbc
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-04-09 10:07:53 +0300

    wip

--- playground/views.py
+++ playground/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.contenttypes.models import ContentType
+from store.models import Product
+from tags.models import TaggedItem
+
+from store.models import Order, OrderItem, Product
 
 
 # Create your views here.
 def say_hello(request):
-    x = 1
-    y = 2
+    
     return render(request, "hello.html", {"name": "django"})
