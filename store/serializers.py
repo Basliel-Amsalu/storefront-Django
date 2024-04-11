68cf5ae337604b2243177ae3bbe7f5891a32d213
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-04-11 17:35:46 +0300

    serializers and views

---
+++ store/serializers.py
@@ -0,0 +1,21 @@
+from decimal import Decimal
+from rest_framework import serializers
+
+from store.models import Product, Collection
+
+
+class CollectionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Collection
+        fields = ["id", "title"]
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ["id", "title", "unit_price", "price_with_tax", "collection"]
+
+    price_with_tax = serializers.SerializerMethodField(method_name="calculateTax")
+
+    def calculateTax(self, product: Product):
+        return product.unit_price * Decimal(1.1)
