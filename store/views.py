fda66afb498f54bae4c63fe42395c9e455465d0a
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-04-12 20:08:11 +0300

    implemented the destroy method instead of the delete

--- store/views.py
+++ store/views.py
@@ -10,7 +10,7 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework import status
 
 from .serializers import CollectionSerializer, ProductSerializer
-from .models import Collection, Product
+from .models import Collection, OrderItem, Product
 
 
 class ProductViewSet(ModelViewSet):
@@ -20,34 +20,25 @@ class ProductViewSet(ModelViewSet):
     def get_serializer_context(self):
         return {"request": self.request}
 
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if OrderItem.objects.filter(product_id=kwargs["pk"]).count() > 0:
             return Response(
-                {
-                    "error": "Product can not be deleted because it is associated with an orderitem"
-                },
+                {"error": "This product cannot be deleted because it is in an order."},
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
-        product.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
 
 
 class CollectionviewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count("products")).all()
     serializer_class = CollectionSerializer
 
-    def get_serializer_context(self):
-        return {"request": self.request}
-
-    def delete(self, request, pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.products.count() > 0:
+    def destroy(self, request, *args, **kwargs):
+        if Product.objects.filter(collection_id=kwargs["pk"]).count() > 0:
             return Response(
                 {
                     "error": "cannot delete this collection because its has related products"
                 },
                 status=status.HTTP_405_METHOD_NOT_ALLOWED,
             )
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request, *args, **kwargs)
