d85b5c84500971308a8cca99f46c52e6659fe32f
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-04-15 10:27:09 +0300

    enabling usage of query parameters

--- store/views.py
+++ store/views.py
@@ -14,8 +14,14 @@ from .models import Collection, OrderItem, Product, Review
 
 
 class ProductViewSet(ModelViewSet):
-    queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+    def get_queryset(self):
+        queryset = Product.objects.all()
+        collection_id = self.request.query_params.get("collection_id")
+        if collection_id is not None:
+            queryset = queryset.filter(collection_id=collection_id)
+        return queryset
 
     def get_serializer_context(self):
         return {"request": self.request}
@@ -45,5 +51,10 @@ class CollectionviewSet(ModelViewSet):
 
 
 class ReviewViewSet(ModelViewSet):
-    queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+
+    def get_queryset(self):
+        return Review.objects.filter(product_id=self.kwargs["product_pk"])
+
+    def get_serializer_context(self):
+        return {"product_id": self.kwargs["product_pk"]}
