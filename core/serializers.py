75e7e4df94dae53f72f355ea137be849576d4c6c
Basliel-Amsalu <acychappiekiller@gmail.com>
2024-04-23 16:00:37 +0300

    custom user profile serializer

--- core/serializers.py
+++ core/serializers.py
@@ -1,6 +1,14 @@
-from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from djoser.serializers import (
+    UserCreateSerializer as BaseUserCreateSerializer,
+    UserSerializer,
+)
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ["id", "username", "email", "password", "first_name", "last_name"]
+
+
+class UserProfileSerializer(UserSerializer):
+    class Meta(UserSerializer.Meta):
+        fields = ["id", "username", "email", "first_name", "last_name"]
--- storefront/settings.py
+++ storefront/settings.py
@@ -149,12 +149,13 @@ REST_FRAMEWORK = {
     ),
 }
 
-SIMPLE_JWT = {
-    "AUTH_HEADER_TYPES": ("JWT",),
-}
+SIMPLE_JWT = {"AUTH_HEADER_TYPES": ("JWT",), "ACCESS_TOKEN_LIFETIME": timedelta(days=1)}
 
 AUTH_USER_MODEL = "core.User"
 
-DJOSER = {"SERIALIZERS": {"user_create": "core.serializers.UserCreateSerializer"}}
-
-SIMPLE_JWT = {"ACCESS_TOKEN_LIFETIME": timedelta(days=1)}
+DJOSER = {
+    "SERIALIZERS": {
+        "user_create": "core.serializers.UserCreateSerializer",
+        "current_user": "core.serializers.UserProfileSerializer",
+    }
+}
